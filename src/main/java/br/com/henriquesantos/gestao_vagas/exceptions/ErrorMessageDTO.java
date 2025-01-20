a4e93db55e2800058590b017b005e5e68f9e74bc
Henrique <henbarros694@gmail.com>
2025-01-20 18:37:41 -0300

    Adiciona classe ErrorMessageDTO para centralizar mensagens de erro

---
+++ src/main/java/br/com/henriquesantos/gestao_vagas/exceptions/ErrorMessageDTO.java
@@ -0,0 +1,11 @@
+package br.com.henriquesantos.gestao_vagas.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessageDTO {
+    private String message;
+    private String field;
+}
