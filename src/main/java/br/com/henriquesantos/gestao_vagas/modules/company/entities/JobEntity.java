2a29dc784e1dae6b03d45f88de9464b7832cc3e6
Henrique <henbarros694@gmail.com>
2025-01-26 13:09:10 -0300

    adicione o relacionamento ManyToOne com CompanyEntity e mapeie a coluna company_id

--- src/main/java/br/com/henriquesantos/gestao_vagas/modules/company/entities/JobEntity.java
+++ src/main/java/br/com/henriquesantos/gestao_vagas/modules/company/entities/JobEntity.java
@@ -5,10 +5,13 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Entity(name = "job")
@@ -23,6 +26,11 @@ public class JobEntity {
     private String benefits;
     private String level;
 
+    @ManyToOne
+    @JoinColumn(name = "company_id", insertable = false, updatable = false)
+    private CompanyEntity companyEntity;
+
+    @Column(name = "company_id")
     private UUID companyId;
 
     @CreationTimestamp
