eb490036b92eec7f7ecbd934eb61e510715d1e31
Henrique <henbarros694@gmail.com>
2025-02-01 12:46:28 -0300

    Comentário deletado

--- src/main/java/br/com/henriquesantos/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/henriquesantos/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.henriquesantos.gestao_vagas.exceptions.UserFoundException;
 import br.com.henriquesantos.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.henriquesantos.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
 import jakarta.validation.Valid;
