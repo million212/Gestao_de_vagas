563aff428ee8c42392a2dd35e363bee8e68573c3
Henrique <henbarros694@gmail.com>
2025-02-24 15:54:16 -0300

    Correção de alguns erros

---
+++ src/main/java/br/com/henriquesantos/gestao_vagas/modules/candidate/dto/AuthCandidateRequestDTO.java
@@ -0,0 +1,9 @@
+package br.com.henriquesantos.gestao_vagas.modules.candidate.dto;
+
+public record AuthCandidateRequestDTO(String username, String password) {
+
+    public static void builder() {
+        // TODO Auto-generated method stub
+    }
+
+}
