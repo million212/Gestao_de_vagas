4613c9290a1c948e79bd4c2a49097c359ab2ba5c
Henrique <henbarros694@gmail.com>
2025-02-24 15:32:56 -0300

    Implementada criptografia de senha usando PasswordEncoder ao salvar candidato

--- src/main/java/br/com/henriquesantos/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/henriquesantos/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -1,6 +1,7 @@
 package br.com.henriquesantos.gestao_vagas.modules.candidate.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.henriquesantos.gestao_vagas.exceptions.UserFoundException;
@@ -13,6 +14,9 @@ public class CreateCandidateUseCase {
     @Autowired
     private CandidateRepository candidateRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidateEntity) {
         this.candidateRepository.findByUsernameOrEmail(
                 candidateEntity.getUsername(),
@@ -20,6 +24,9 @@ public class CreateCandidateUseCase {
                 .ifPresent((user) -> {
                     throw new UserFoundException();
                 });
+
+        var password = passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(password);
 
         return this.candidateRepository.save(candidateEntity);
     }
