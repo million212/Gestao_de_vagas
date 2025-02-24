97534f5a25b4aca107903111ebc093e24b8962fb
Henrique <henbarros694@gmail.com>
2025-02-24 15:43:51 -0300

    Implementado caso de uso para autenticação de candidatos com validação de credenciais e geração de token JWT

---
+++ src/main/java/br/com/henriquesantos/gestao_vagas/modules/candidate/useCases/usernameNotFoundException.java
@@ -0,0 +1,8 @@
+package br.com.henriquesantos.gestao_vagas.modules.candidate.useCases;
+
+public class usernameNotFoundException {
+
+    public usernameNotFoundException(String string) {
+    }
+
+}
