f19b1011b318a5f837f5dd4599b9e9d427df63e6
Henrique <henbarros694@gmail.com>
2025-03-04 11:53:54 -0300

    Mudança na ordem dos RequestMapping

--- src/main/java/br/com/henriquesantos/gestao_vagas/modules/company/controllers/AuthCompanyController.java
+++ src/main/java/br/com/henriquesantos/gestao_vagas/modules/company/controllers/AuthCompanyController.java
@@ -12,13 +12,13 @@ import br.com.henriquesantos.gestao_vagas.modules.candidate.useCases.AuthCompany
 import br.com.henriquesantos.gestao_vagas.modules.company.dto.AuthCompanyDTO;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/company")
 public class AuthCompanyController {
 
     @Autowired
     private AuthCompanyUseCase authCompanyUseCase;
 
-    @PostMapping("/company")
+    @PostMapping("/auth")
     public ResponseEntity<Object> create(@RequestBody AuthCompanyDTO authCompanyDTO) {
         try {
             var result = this.authCompanyUseCase.execute(authCompanyDTO);
